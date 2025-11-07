6194ee0649dd6cfaf42e4bcbaa1bac6d8130acd6
Your Name <you@example.com>
2025-11-07 17:05:03 +0100

    Fill in results from a tab that contains available cars

---
+++ assets/js/base-vehicules-disponibles.js
@@ -0,0 +1,30 @@
+export const baseVehiculesDisponibles = [
+    {
+        url: './assets/img/vehicule1.png',
+        modele: 'Peugeot 208',
+        description: 'Diesel, 5 portes, GPS, AutoRadio, Forfait 1000km (0,5/km supplémentaire).',
+        prix: 999,
+        agence: 'Agence de Paris'
+    },
+    {
+        url: 'assets/img/vehicule2.png',
+        modele: 'Ford Focus',
+        description: 'Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0,5/km supplémentaire).',
+        prix: 999,
+        agence: ''    
+    },
+    {
+        url: 'assets/img/vehicule4.png',
+        modele: 'Audi A1',
+        description: 'Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0,55/km supplémentaire).',
+        prix: 1100,
+        agence: 'Agence de Paris'
+    },
+    {
+        url: 'assets/img/vehicule4.png',
+        modele: 'Opel Mokka',
+        description: 'Diesel, 5 portes, GPS, AutoRadio, Forfait 1000 km (0,4/km supplémentaire).',
+        prix: 1150,
+        agence: 'Agence de Paris'
+    }
+];
---
+++ assets/js/script.js
@@ -0,0 +1,88 @@
+import { baseVehiculesDisponibles } from "./base-vehicules-disponibles.js";
+
+const articlesContainer = document.getElementById('articles-container');
+
+console.log('merde');
+
+baseVehiculesDisponibles.forEach((vehicule) => {
+    console.log('Have : ' + vehicule.modele);
+
+    const article = document.createElement('article');
+
+    /*
+     * Creation de la zone d'affichage du vehicule
+     */
+
+    const photosVehicule = document.createElement('div');
+    photosVehicule.classList.add('photos-vehicule')
+
+    /* Creation de la fleche de gauche */
+    const div1 = document.createElement('div');
+    const img1 = document.createElement('img');
+    img1.src = "./assets/icon/arrow_left.svg";
+    div1.append(img1);
+    photosVehicule.append(div1);
+
+    /* Creation de l'image du vehicule */
+    const div2 = document.createElement('div');
+    const img2 = document.createElement('img');
+    img2.src = vehicule.url;
+    div2.append(img2);
+    photosVehicule.append(div2);
+
+    /* Creation de la fleche de droite */
+    const div3 = document.createElement('div');
+    const img3 = document.createElement('img');
+    img3.src = "./assets/icon/arrow_right.svg";
+    div3.append(img3);
+    photosVehicule.append(div3);
+
+    /*
+     * Creation de la zone d'informations sur le vehicule
+     */
+
+    //  Ajout du modele 
+    const divInfos = document.createElement('div');
+    const modele = document.createElement('div');
+    modele.classList.add('modele');
+    modele.innerText = vehicule.modele;
+    divInfos.append(modele);
+
+    // Ajout de la description
+    const description = document.createElement('div');
+    description.classList.add('description');
+    description.innerText = vehicule.description;
+    divInfos.append(description);
+
+    // Ajout du prix et de l'agence
+    const prixAgence = document.createElement('div');
+    // Ajout du prix
+    prixAgence.classList.add('prix-agence');
+    const prix = document.createElement('div');
+    prix.classList.add('prix');
+    prix.innerText = vehicule.prix.toString() + '€';
+    prixAgence.append(prix);
+    const divIntercalaire = document.createElement('div');
+    divIntercalaire.innerText = '-';
+    prixAgence.append(divIntercalaire);
+    const agence = document.createElement('agence');
+    agence.classList.add('agence');
+    agence.innerText = vehicule.agence;
+    prixAgence.append(agence);
+    divInfos.append(prixAgence);
+
+    // Ajout du bouton reserver
+    const button = document.createElement('button');
+    button.classList.add('reserver');
+    button.innerText = 'Réserver et Payer';
+    divInfos.append(button);
+
+    // Ajout des éléments dans l'article
+
+    article.append(photosVehicule);
+    article.append(divInfos);
+
+    // Ajout de l'article dans la page
+    articlesContainer.append(article);
+});
+
